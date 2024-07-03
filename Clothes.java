888f77fadb644fe266eff98ff8cd70e74b1042aa
sourabhaxworkz <sourabhayn@gmail.com>
2024-07-03 10:25:51 +0530

    125

---
+++ Clothes.java
@@ -0,0 +1,13 @@
+class Clothes{
+	public static void main(String[] hM){
+	
+	String clotheName = "Liva";
+	int price = 1500;
+	String clotheColor = "Red";
+	
+		System.out.println("The clotheName is " + clotheName);
+		System.out.println("The price is " + price);
+		System.out.println("The color is " + clotheColor);
+	}
+
+}
